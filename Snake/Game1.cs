5a296fbd697149d2b5540506f9c476f64bff02f9
Jakob Trigger Knop <jakikob@gmail.com>
2026-02-22 12:05:14 +0100

    first commit

---
+++ Snake/Game1.cs
@@ -0,0 +1,40 @@
+﻿using GMDCore;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Snake;
+
+public class Game1 : Core
+{
+    public const int VirtualWidth = 512;
+    public const int VirtualHeight = 288;
+
+    public Game1() : base("Snake", 1280, 720, VirtualWidth, VirtualHeight)
+    {
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        SpriteBatch.Begin(transformMatrix: ScreenScaleMatrix);
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
